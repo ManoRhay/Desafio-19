861801b1e5703c9e74c65f3640391ff817617967
Rhayan Adriell <rhayanadriell57@outlook.com>
2024-05-12 18:50:04 -0300

    Desafio19

---
+++ javinha.js
@@ -0,0 +1,6 @@
+var cor=prompt("seleção de cor. DISCLAIMER -> Deve ser em inglês por gentileza.")
+    if (cor==null){
+    } else {
+        document.body.style.backgroundColor=cor
+        location.reload()
+    }
